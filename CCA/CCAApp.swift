bf0c998132f3d2acaec1319e4ebd8de1eda01f1e
Vahtee Boo <vahtee.boo@gmail.com>
2022-02-02 13:13:53 +0200

    Initial Commit

---
+++ CCA/CCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCAApp.swift
+//  CCA
+//
+//  Created by Vahtee Boo on 02.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
